d6f9f5389b5f0a0de85cf93a27d63ae1fd65da1d
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2020-01-03 19:48:49 +0200

    Added Parallel Requests execution test

--- Tests/LegatusTests/ApiClientTests.swift
+++ Tests/LegatusTests/ApiClientTests.swift
@@ -22,6 +22,37 @@ final class ApiClientTests: XCTestCase {
         wait(for: [randomUserApiExpectation], timeout: 10.0)
     }
 
+    func testParallelRequests() {
+        let randomUserApiClient = APIClient(baseURL: URL(string: "https://randomuser.me/api/")!)
+        let randomUserApiRequest = RandomUserApiRequest()
+        let firstRequestExpectation = XCTestExpectation(description: "Execute first randomuser api request.")
+        let secondRequestExpectation = XCTestExpectation(description: "Execute second randomuser api request.")
+
+        randomUserApiClient.executeRequest(request: randomUserApiRequest) { result in
+            if case let .success(fetchedUsers) = result {
+                XCTAssertFalse(fetchedUsers.isEmpty)
+                XCTAssertTrue(fetchedUsers.count == 1)
+                XCTAssertNotNil(fetchedUsers.first?.firstName)
+                XCTAssertNotNil(fetchedUsers.first?.lastName)
+                XCTAssertNotNil(fetchedUsers.first?.email)
+            }
+            firstRequestExpectation.fulfill()
+        }
+
+        randomUserApiClient.executeRequest(request: randomUserApiRequest) { result in
+            if case let .success(fetchedUsers) = result {
+                XCTAssertFalse(fetchedUsers.isEmpty)
+                XCTAssertTrue(fetchedUsers.count == 1)
+                XCTAssertNotNil(fetchedUsers.first?.firstName)
+                XCTAssertNotNil(fetchedUsers.first?.lastName)
+                XCTAssertNotNil(fetchedUsers.first?.email)
+            }
+            secondRequestExpectation.fulfill()
+        }
+
+        wait(for: [firstRequestExpectation, secondRequestExpectation], timeout: 10.0)
+    }
+
     func testErrorResponse() {
         let apiClient = APIClient(baseURL: URL(string: "https://webservice.com/api/")!)
         let randomUserApiRequest = RandomUserApiRequest()
@@ -39,6 +70,7 @@ final class ApiClientTests: XCTestCase {
 
     static var allTests = [
         ("testNotEmptyResponse", testNotEmptyResponse),
+        ("testParallelRequests", testParallelRequests),
         ("testErrorResponse", testErrorResponse)
     ]
 }
